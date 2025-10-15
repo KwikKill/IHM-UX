452ca88e1c37cbb1edbffe8081f25bb925003ac8
Paul <79766427+TheBloodMan49@users.noreply.github.com>
2025-10-15 12:05:24 +0200

    Header start

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,5 +1,14 @@
 <template>
-    <div class="header">
-        <h1>My Application Header</h1>
+    <div class="header flex justify-between items-center p-4 bg-primary text-white gap-2">
+        <h1>Swish</h1>
+        <nav class="flex-1 flex justify-center gap-4">
+            <NuxtLink to="/" class="hover:underline">Accueil</NuxtLink>
+            <NuxtLink to="/search" class="hover:underline">Recherche</NuxtLink>
+            <NuxtLink to="/map" class="hover:underline">Autour de moi</NuxtLink>
+        </nav>
+        <div class="user-info">
+            <span>Utilisateur</span>
+            <button class="logout-button">Déconnexion</button>
+        </div>
     </div>
 </template>
