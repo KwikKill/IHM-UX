b00edc02362529ec6ed49a791da82a85b9fd4890
Paul <79766427+TheBloodMan49@users.noreply.github.com>
2025-10-22 12:09:45 +0200

    alignments

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,13 +1,23 @@
 <template>
-    <div class="footer bg-card text-primary flex">
+    <div class="footer bg-card text-primary grid grid-cols-3">
         <div>
             <span>Copyright Sysrez(TM) 2025</span>
         </div>
-        <div class="flex-1 text-center">
-            <span>Plan du site | Contact</span>
+        <div class="flex justify-center gap-2">
+            <div>
+                <NuxtLink to="/" class="">Plan du site</NuxtLink>
+            </div>
+            <div>
+                <NuxtLink to="/contact" class="">Contact</NuxtLink>
+            </div>
         </div>
-        <div class="text-right">
-            <span>Mentions légales | Politique de confidentialité</span>
+        <div class="flex justify-end gap-2">
+            <div>
+                <NuxtLink to="/terms" class="">Conditions d'utilisation</NuxtLink>
+            </div>
+            <div>
+                <NuxtLink to="/privacy" class="">Politique de confidentialité</NuxtLink>
+            </div>
         </div>
     </div>
 </template>
--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,12 +1,14 @@
 <template>
-    <div class="header flex justify-between items-center p-4 bg-card text-primary gap-2 sticky top-0">
-        <NuxtLink to="/" class="text-2xl font-bold"><Icon name="material-symbols:format-letter-spacing-wider" size="2rem"/></NuxtLink>
-        <nav class="flex-1 flex justify-center gap-4">
+    <div class="header grid grid-cols-3 p-4 items-center bg-card text-primary gap-2 sticky top-0">
+        <div>
+            <NuxtLink to="/" class="text-3xl font-bold"><Icon name="material-symbols:format-letter-spacing-wider" size="2rem"/></NuxtLink>
+        </div>
+        <nav class="flex justify-center gap-4">
             <NuxtLink to="/" class="hover:underline">Accueil</NuxtLink>
             <NuxtLink to="/search" class="hover:underline">Recherche</NuxtLink>
             <NuxtLink to="/map" class="hover:underline">Autour de moi</NuxtLink>
         </nav>
-        <div class="user-info">
+        <div class="flex justify-end gap-2">
             <span>Utilisateur</span>
             <button class="logout-button">Déconnexion</button>
         </div>
